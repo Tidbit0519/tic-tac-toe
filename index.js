4f68f2682908a2d09cd042faa7795733c89af5e0
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2024-04-25 15:32:21 -1000

    Implement styling and animation all over

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const Gameboard = () => {
 		}
 	}
 
-	const placeMark = (row, column, mark) => {
-		board[row][column].addMark(mark)
+	const placeMark = (row, column, mark, color) => {
+		board[row][column].addMark(mark, color)
 	}
 
 	const getBoard = () => {
@@ -26,26 +26,33 @@ const Gameboard = () => {
 
 const Cell = () => {
 	let value = ""
+	let cellColor = ""
 
-	const addMark = (mark) => {
+	const addMark = (mark, color) => {
 		value = mark
+		cellColor = color
 	}
 
 	const getValue = () => {
 		return value
 	}
 
+	const getColor = () => {
+		return cellColor
+	}
+
 	return {
 		addMark,
 		getValue,
+		getColor,
 	}
 }
 
 const GameController = () => {
 	let gameboard = Gameboard()
 	const players = [
-		{ name: "Player 1", mark: "X" },
-		{ name: "Player 2", mark: "O" },
+		{ name: "Player 1", mark: "X", color: "red"},
+		{ name: "Player 2", mark: "O", color: "blue"},
 	]
 	let currentPlayer = players[0]
 
@@ -59,7 +66,7 @@ const GameController = () => {
 
 	const playRound = (row, column) => {
 		if (gameboard.getBoard()[row][column].getValue() === "") {
-			gameboard.placeMark(row, column, currentPlayer.mark)
+			gameboard.placeMark(row, column, currentPlayer.mark, currentPlayer.color)
 			switchPlayer()
 		}
 	}
@@ -78,7 +85,6 @@ const GameController = () => {
 				board[i][0] === board[i][1] &&
 				board[i][0] === board[i][2]
             ) {
-                console.log("Found a winner!")
 				winner = players.find(
 					(player) => player.mark === board[i][0]
 				)
@@ -111,7 +117,12 @@ const GameController = () => {
 			board[0][2] === board[2][0]
 		) {
 			winner = players.find((player) => player.mark === board[0][2])
-        }
+		}
+		
+		if (winner) {
+			const restartButton = document.querySelector(".restart-btn")
+			restartButton.style.display = "block"
+		}
 
 		return winner
     }
@@ -136,46 +147,59 @@ function main() {
 	const gameBoardDiv = document.querySelector(".game-board")
 
     const updatePlayerTurn = () => {
-		const winner = gameController.checkWinner()
-		playerTurnDiv.style.width = "0.1%"
-		if (winner) {
-			playerTurnDiv.textContent = `${winner.name} win!`
-		} else if (
-			gameController
-				.getBoard()
-				.flat()
-				.every((cell) => cell.getValue() !== "")
-		) {
-			playerTurnDiv.textContent = "Draw!"
-		} else {
-			playerTurnDiv.textContent = `${
-				gameController.getCurrentPlayer().name
-			}'s Turn`
+			const winner = gameController.checkWinner()
+			playerTurnDiv.style.animation = "none" 
+			playerTurnDiv.style.width = "1%" 
+			playerTurnDiv.offsetHeight // This does nothing but force a layout reflow
+
+			if (winner) {
+				playerTurnDiv.textContent = `${winner.name} wins!`
+			} else if (
+				gameController
+					.getBoard()
+					.flat()
+					.every((cell) => cell.getValue() !== "")
+			) {
+				playerTurnDiv.textContent = "Draw!"
+			} else {
+				playerTurnDiv.textContent = `${
+					gameController.getCurrentPlayer().name
+				}'s Turn`
+			}
+
+			// Timeout to ensure the animation is reapplied after reflow
+			setTimeout(() => {
+				playerTurnDiv.style.animation =
+					"typing 1s steps(40, end) forwards, blink-caret .75s step-end infinite"
+			}, 10)
 		}
-		playerTurnDiv.style.animation =
-			"typing 2s steps(40, end) forwards, blink-caret .75s step-end infinite"
-	}
+
 
 	const updateGameBoard = () => {
-		const board = gameController.getBoard()
 		gameBoardDiv.innerHTML = ""
+		const board = gameController.getBoard()
 		board.forEach((row, rowIndex) => {
 			const rowDiv = document.createElement("div")
 			rowDiv.classList.add("row")
 			row.forEach((cell, columnIndex) => {
 				const cellButton = document.createElement("button")
 				cellButton.classList.add("cell")
+				cellButton.id = `cell-${rowIndex}-${columnIndex}`
 				cellButton.dataset.row = rowIndex
 				cellButton.dataset.column = columnIndex
 				cellButton.textContent = cell.getValue()
+				cellButton.style.color = cell.getColor()
 				cellButton.addEventListener("click", cellClick)
 				rowDiv.appendChild(cellButton)
+
 			})
 			gameBoardDiv.appendChild(rowDiv)
 		})
 	}
 
 	function cellClick(event) {
+		if (gameController.checkWinner() || playerTurnDiv.textContent === "Draw!" ) return
+			
 		const row = event.target.dataset.row
 		const column = event.target.dataset.column
 		gameController.playRound(row, column)
@@ -183,30 +207,23 @@ function main() {
 		updatePlayerTurn()
     }
     
-    // function restartGame() {
-    //     const restartButton = document.querySelector(".restart-btn")
-    //     restartButton.addEventListener("click", () => {
-    //         console.log("Restarting game")
-    //         gameController.restartGame()
-    //     })
-    // }
-
-    // restartGame()
 	updatePlayerTurn()
-	updateGameBoard()
+	setTimeout(() => {
+		gameBoardDiv.style.animation =
+			"fade-in 1s cubic-bezier(0.390, 0.575, 0.565, 1.000) forwards"
+		updateGameBoard()
+	}, 10)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 	var gameTitle = document.querySelector(".game-title")
-	var playerTurn = document.querySelector(".player-turn")
-
 	gameTitle.addEventListener("animationend", function (e) {
 		if (e.animationName === "typing") {
 			gameTitle.style.animation = "none"
 			gameTitle.style.borderRight = "none"
-			setTimeout(function () {
+			setTimeout(() => {
 				main()
-			}, 500)
+			}, 100)
 		}
 	})
 })
